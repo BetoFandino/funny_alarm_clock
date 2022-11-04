e2646a7182e72f52174030e944838f1fc610555d
jfandinoRSE <beto3971@gmail.com>
2022-11-04 01:59:29 -0400

    feat! : new interface for the home page and clock funtionalities

---
+++ funny_alarm_clock/lib/home/ui/home.dart
@@ -0,0 +1,89 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../widgets/shapes_painter.dart';
+
+class Home extends StatefulWidget {
+  const Home({key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+
+   String? _timeString;
+   TabController? _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 4, vsync: this,
+    initialIndex: 0
+    );
+
+    _timeString = _formatDateTime(DateTime.now());
+
+    Timer.periodic(Duration(seconds: 1), (Timer t) => _getTime());
+    
+  }
+
+  void _getTime(){
+    final DateTime now = DateTime.now();
+    final String formattedDateTime = _formatDateTime(now);
+
+    setState(() {
+      _timeString = formattedDateTime;
+    });
+  }
+
+  String _formatDateTime(DateTime dateTime){
+    return DateFormat('hh:mm').format(dateTime);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(length: 4, 
+      child: Scaffold(
+        appBar:AppBar(
+          backgroundColor: Theme.of(context).primaryColor,
+          bottom: TabBar(
+            controller: _tabController,
+            indicatorColor: Theme.of(context).accentColor,
+            indicatorWeight: 4.0,
+            tabs: <Widget>[
+              Tab(icon: Icon(Icons.access_time), text: 'Clock'),
+              Tab(icon: Icon(Icons.alarm), text: 'Alarm'),
+              Tab(icon: Icon(Icons.hourglass_empty), text: 'Timer'),
+              Tab(icon: Icon(Icons.av_timer), text: 'Stopwatch'),
+            ],
+          )
+        ),
+        body: Container(
+          color: Theme.of(context).primaryColor,
+          child:  TabBarView(
+            controller: _tabController,
+            children: <Widget>[
+              Container(
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: CustomPaint(
+                        painter: ShapesPainter(),
+                        child: Container(height: 500.0,),
+
+                        ),
+                    )
+                  ],
+                ),
+              )
+            ]
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ funny_alarm_clock/lib/home/widgets/shapes_painter.dart
@@ -0,0 +1,45 @@
+import 'dart:ffi';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math.dart';
+import 'package:vector_math/vector_math_64.dart' as Vector;
+import 'dart:ui' as ui;
+
+class ShapesPainter extends CustomPainter {
+  
+  @override
+  void paint(Canvas canvas, ui.Size size){
+
+    // Global angle
+    var angle = Vector.radians(-90);
+
+    final double r = sqrt(size.width * size.width + size.width * size.height) / 2;
+    final alpha = atan(size.height / size.width);
+    final beta = alpha + angle;
+    final shifty = r * sin(beta);
+    final shiftx = r * cos(beta);
+    final translateX = size.width /2 -shiftx;
+    final translateY = size.height / 2 - shifty;
+    canvas.translate(translateX, translateY);
+    canvas.rotate(angle);
+
+    DateTime now = DateTime.now();
+
+    // init painter
+    final paint = Paint();
+    var center = Offset(size.width / 2, size.height / 2);
+    paint.color = Color.fromARGB(255, 255, 255, 255);
+    canvas.drawCircle(center, (size.width /3) - 5, paint);
+    paint.strokeCap = StrokeCap.round;
+
+    
+
+
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate){
+    return true;
+  }
+}
